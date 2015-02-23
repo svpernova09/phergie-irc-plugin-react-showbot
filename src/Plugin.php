6b5e030aacce5aa790437ae2f26a292f5ab2e60a
Joe Ferguson <joe@joeferguson.me>
2015-02-23 16:30:46 -0600

    Add help command

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,7 +2,7 @@
 /**
  * Phergie plugin for Support a live event in an irc channel (http://www.joeferguson.me)
  *
- * @link https://github.com/phergie/phergie-irc-plugin-react-showbot for the canonical source repository
+ * @link https://github.com/svpernova09/phergie-irc-plugin-react-showbot for the canonical source repository
  * @copyright Copyright (c) 2015 Joe Ferguson (http://www.joeferguson.me)
  * @license http://phergie.org/license New BSD License
  * @package Phergie\Irc\Plugin\React\Showbot
@@ -22,39 +22,64 @@ use Phergie\Irc\Plugin\React\Command\CommandEvent as Event;
  */
 class Plugin extends AbstractPlugin
 {
-    /**
-     * Accepts plugin configuration.
-     *
-     * Supported keys:
-     *
-     *
-     *
-     * @param array $config
-     */
-    public function __construct(array $config = array())
-    {
+	/**
+	 * Accepts plugin configuration.
+	 *
+	 * Supported keys:
+	 *
+	 *
+	 *
+	 * @param array $config
+	 */
+	public function __construct(array $config = array())
+	{
 
-    }
+	}
 
-    /**
-     *
-     *
-     * @return array
-     */
-    public function getSubscribedEvents()
-    {
-        return array(
-            'command.' => 'handleCommand',
-        );
-    }
+	/**
+	 *
+	 *
+	 * @return array
+	 */
+	public function getSubscribedEvents()
+	{
+		return array(
+			'command.sb.help' => 'handleCommandHelp',
+		);
+	}
 
-    /**
-     *
-     *
-     * @param \Phergie\Irc\Plugin\React\Command\CommandEvent $event
-     * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
-     */
-    public function handleCommand(Event $event, Queue $queue)
-    {
-    }
+	/**
+	 *
+	 *
+	 * @param \Phergie\Irc\Plugin\React\Command\CommandEvent $event
+	 * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
+	 */
+	public function handleCommand(Event $event, Queue $queue)
+	{
+	}
+
+
+	/**
+	 * Handle the help command
+	 *
+	 * @param \Phergie\Irc\Plugin\React\Command\CommandEvent $event
+	 * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
+	 */
+	public function handleCommandHelp(Event $event, Queue $queue)
+	{
+		$this->sendIrcResponse($event, $queue, $this->getHelpLines());
+	}
+
+	/**
+	 * Return an array of help command response lines
+	 *
+	 * @return array
+	 */
+	public function getHelpLines()
+	{
+		return array(
+			'Usage: sb <action>',
+			'actions: help, next, schedule, suggest'
+		);
+	}
 }
