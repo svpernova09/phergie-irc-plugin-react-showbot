44703e79b0a52bfb7798c98bea82110d7cd2679c
Joe Ferguson <joe@joeferguson.me>
2015-02-23 17:10:16 -0600

    Add tests for testing help command and mock stuff for phake use.

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -24,6 +24,60 @@ use Phergie\Irc\Plugin\React\Showbot\Plugin;
 class PluginTest extends \PHPUnit_Framework_TestCase
 {
 
+	/**
+	 * Data provider for testHandleHelp().
+	 *
+	 * @return array
+	 */
+	public function dataProviderHandleHelp()
+	{
+		$data = array();
+		$methods = array(
+			'handleCommandHelp'
+		);
+		foreach ($methods as $method) {
+			$data[] = array($method);
+		}
+		return $data;
+	}
+
+	/**
+	 * Tests handleCommand().
+	 *
+	 * @param string $method
+	 * @dataProvider dataProviderHandleHelp
+	 */
+	public function testHandleCommand($method)
+	{
+		$event = $this->getMockCommandEvent();
+		Phake::when($event)->getCustomParams()->thenReturn(array());
+		Phake::when($event)->getSource()->thenReturn('#channel');
+		Phake::when($event)->getCommand()->thenReturn('PRIVMSG');
+		$queue = $this->getMockEventQueue();
+		$plugin = new Plugin;
+		$plugin->$method($event, $queue);
+		Phake::verify($queue, Phake::atLeast(1))
+		     ->ircPrivmsg('#channel', $this->isType('string'));
+	}
+
+	/**
+	 * Tests handleCommandHelp().
+	 *
+	 * @param string $method
+	 * @dataProvider dataProviderHandleHelp
+	 */
+	public function testHandleCommandHelp($method)
+	{
+		$event = $this->getMockCommandEvent();
+		Phake::when($event)->getCustomParams()->thenReturn(array());
+		Phake::when($event)->getSource()->thenReturn('#channel');
+		Phake::when($event)->getCommand()->thenReturn('PRIVMSG');
+		$queue = $this->getMockEventQueue();
+		$plugin = new Plugin;
+		$plugin->$method($event, $queue);
+		Phake::verify($queue, Phake::atLeast(1))
+		     ->ircPrivmsg('#channel', $this->isType('string'));
+	}
 
     /**
      * Tests that getSubscribedEvents() returns an array.
@@ -33,4 +87,24 @@ class PluginTest extends \PHPUnit_Framework_TestCase
         $plugin = new Plugin;
         $this->assertInternalType('array', $plugin->getSubscribedEvents());
     }
+
+	/**
+	 * Returns a mock command event.
+	 *
+	 * @return \Phergie\Irc\Plugin\React\Command\CommandEvent
+	 */
+	protected function getMockCommandEvent()
+	{
+		return Phake::mock('Phergie\Irc\Plugin\React\Command\CommandEvent');
+	}
+
+	/**
+	 * Returns a mock event queue.
+	 *
+	 * @return \Phergie\Irc\Bot\React\EventQueueInterface
+	 */
+	protected function getMockEventQueue()
+	{
+		return Phake::mock('Phergie\Irc\Bot\React\EventQueueInterface');
+	}
 }
